fb448e84b0c976fffbc1c2e97b791827fe8d57a3
hasover <hasover@gmail.com>
2021-06-05 19:51:50 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -0,0 +1,30 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    private ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+       pool.execute(() -> this.send(
+               String.format("Notification %s to email %s", user.getUsername(), user.getEmail()),
+               String.format("Add a new event to %s", user.getUsername()),
+               user.getEmail()));
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/pool/User.java
@@ -0,0 +1,23 @@
+package ru.job4j.pool;
+
+import net.jcip.annotations.Immutable;
+
+@Immutable
+public class User {
+    private final String username;
+    private final String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
